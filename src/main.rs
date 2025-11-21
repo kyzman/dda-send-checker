5be9f0cd38c623ff3b15ec2a5876a958dd508ac8
kyzman <kyzwork@yandex.ru>
2025-11-21 16:04:48 +0500

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Starting check.");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 1. Загрузка переменных из .env
+    dotenv::dotenv().map_err(|e| format!("Failed to load .env: {}", e))?;
+
+    Ok(())
 }
