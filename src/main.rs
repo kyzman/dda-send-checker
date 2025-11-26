5dced7272bfb65448247f45d94fbfc898de18690
kyzman <kyzwork@yandex.ru>
2025-11-27 00:25:53 +0500

    implement example from https://dev.to/bbkr/ssh-port-forwarding-from-within-rust-code-5an

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use russh::{self, client, keys::PrivateKeyWithHashAlg, keys::load_secret_key, keys::ssh_key};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use sqlx::{Executor, PgPool};
 use std::sync::Arc;
 use tokio;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 struct Client {}
 
@@ -65,11 +64,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         None,
     );
 
+    // 4. Запуск TCP-слушателя на локальном порту
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", mysql_local_port)).await?;
+    println!("✅ Listening local port {}", mysql_local_port);
+
     // 4. Подключение к SSH-серверу
     let sh = Client {};
     let addr = format!("{}:{}", ssh_host, ssh_port);
     let config = russh::client::Config::default();
     let config = Arc::new(config);
+
     let mut handle = client::connect(config, addr, sh)
         .await
         .map_err(|e| format!("⛔ Failed to connect to SSH server: {}", e))?;
@@ -82,70 +86,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(e) => return Err(format!("⛔ SSH authentication failed: {}", e).into()),
     }
 
-    let local_addr = SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-        mysql_local_port.into(),
+    // 6. Открываем канал от локальной сессии до сервиса.
+    tokio::spawn(async move {
+        let (mut local_socket, _) = listener
+            .accept()
+            .await
+            .expect("⛔ Cannot process local client");
+
+        let ssh_channel = handle
+            .channel_open_direct_tcpip(
+                &mysql_remote_host.clone(),
+                mysql_remote_port as u32,
+                "127.0.0.1",
+                mysql_local_port as u32,
+            )
+            .await
+            .expect("⛔ Cannot open SSH forwarding channel");
+        let mut ssh_stream = ssh_channel.into_stream();
+
+        // 7. Копирование в обе стороны данных.
+        tokio::io::copy_bidirectional(&mut local_socket, &mut ssh_stream)
+            .await
+            .expect("⛔ Copy error between local socket and SSH stream");
+    });
+
+    // 8. Подключаемся к Базе данных через созданный туннель
+    let database_url = format!(
+        "postgres://{}:{}@localhost:{}/{}",
+        mysql_user, mysql_password, mysql_local_port, mysql_db
     );
 
-    let listener = tokio::net::TcpListener::bind(local_addr).await?;
-    let (socket, o_addr) = listener.accept().await?;
-    let mut stream = socket;
-    let mut channel = handle
-        .channel_open_direct_tcpip(
-            mysql_remote_host.to_string(),
-            mysql_remote_port.into(),
-            o_addr.ip().to_string(),
-            o_addr.port().into(),
-        )
-        .await?;
-    
-    let mut stream_closed = false;
-    let mut buf = vec![0; 65536];
-    loop {
-        // Handle one of the possible events:
-        tokio::select! {
-            // There's socket input available from the client
-            r = stream.read(&mut buf), if !stream_closed => {
-                match r {
-                    Ok(0) => {
-                        stream_closed = true;
-                        channel.eof().await?;
-                    },
-                    // Send it to the server
-                    Ok(n) => channel.data(&buf[..n]).await?,
-                    Err(e) => return Err(e.into()),
-                };
-            },
-            // There's an event available on the session channel
-            Some(msg) = channel.wait() => {
-                match msg {
-                    // Write data to the client
-                    russh::ChannelMsg::Data { ref data } => {
-                        stream.write_all(data).await?;
-                    }
-                   russh::ChannelMsg::Eof => {
-                        if !stream_closed {
-                            channel.eof().await?;
-                        }
-                        break;
-                    }
-                    russh::ChannelMsg::WindowAdjusted { new_size:_ }=> {
-                        // Ignore this message type
-                    }
-                    _ => {todo!()}
-                }
-            },
-        }
-    }
+    let pool = PgPool::connect(&database_url).await?;
 
-    // Отключение
-    match handle
-        .disconnect(russh::Disconnect::ByApplication, "finished", "English")
-        .await
-    {
-        Ok(_) => println!("✅ Successfully disconnected from host {}", ssh_host),
-        Err(e) => return Err(format!("⛔ disconnection failed: {}", e).into()),
-    }
+    // 9. Пример запроса к БД.
+    let row = pool.fetch_all("SELECT VERSION() as version").await.unwrap();
+
+    // 10. Тестовый вывод результата.
+    println!("Version: {:?}", row);
+
+    // Отключение канала.
+    // handle
+    //     .cancel_tcpip_forward(&mysql_remote_host, mysql_remote_port.into())
+    //     .await?;
+
+    // Отключение SSH
+    // match handle
+    //     .disconnect(russh::Disconnect::ByApplication, "finished", "English")
+    //     .await
+    // {
+    //     Ok(_) => println!("✅ Successfully disconnected from host {}", ssh_host),
+    //     Err(e) => return Err(format!("⛔ disconnection failed: {}", e).into()),
+    // }
 
     Ok(())
 }
